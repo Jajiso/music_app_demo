5252baf9d1836573ffbb5ee3431df551df9f4a7f
Jajiso <jajiso123@gmail.com>
2020-12-22 20:46:16 +0000

    added Navigation Components to the app

--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -2,17 +2,14 @@ package com.example.musicapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.musicapp.ui.main.MainFragment
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
-        }
+
+        findNavController(R.id.nav_host_fragment)
     }
 }
